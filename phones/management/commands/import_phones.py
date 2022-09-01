bdba3f868c077505d746829dc0bb16d9f3d7d1c1
Vasenin-Sashko <vasenin.as@yandex.ru>
2022-09-01 16:52:10 +0300

    Сделана основная задача

---
+++ phones/management/commands/import_phones.py
@@ -0,0 +1,29 @@
+import csv
+from django.template.defaultfilters import slugify
+from django.core.management.base import BaseCommand
+from phones.models import Phone
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        with open('phones.csv', 'r') as file:
+        # TODO: Добавьте сохранение модели
+            phones = csv.reader(file, delimiter=';')
+            # пропускаем заголовок
+            next(phones)
+
+            for phone in phones:
+                new_phone = Phone.objects.create(
+                    id=int(phone[0]),
+                    name=phone[1],
+                    image=phone[2],
+                    price=int(phone[3]),
+                    release_date=phone[4],
+                    lte_exists=phone[5],
+                    slug=slugify(phone[1]),
+                )
+        pass
+        
